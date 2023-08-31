05ccd72e80ecd16ef8fa855b2445a31098a46a1b
MIDKINGIELIA <qkrrlqja7876@naver.com>
2023-08-31 16:19:45 +0900

    if와 getchar puchat 사용법

--- 0831.c
+++ 0831.c
@@ -1,10 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main(void){
-	int i;
-	scanf("%d", &i);
-	printf("hellow world");
-	printf("이건 두번째 컴퓨터에서 실행하는 테스트 코드");
+	char ch;
+	//ch = getchar();
+	/*putchar(ch);
+	*/
+	while ((ch = getchar()) != '\n'){
+		for (int i = '0'; i < '10'; i++) { /*'0'과 '9'는 아스키 코드라서 예를들면 '0'이 65면 '9'는 74이므로 저논리가 */
+			if (ch == i) ch = '*';		    /*가능하다. 좀더 간단하게 만들자면 if(ch >='0' && ch<='9') 로도 가능하다.*/
+				putchar(ch);
+		}
+		//if (ch == 'f' || 'F')
+		//	putchar('x');
+		//else
+		//	putchar(ch);
+	}
 	return 0;
 }
 
